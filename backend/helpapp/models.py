e1b284879fa1312d22580257594d4eb0042dfdbd
Paulo <thereisnotruth12@gmail.com>
2021-04-11 23:29:18 +0900

    modify model fields

--- backend/helpapp/models.py
+++ backend/helpapp/models.py
@@ -14,7 +14,7 @@ class User(models.Model):
     point = models.IntegerField()
 
 class Study(models.Model):
-    study_id = models.IntegerField(primary_key=models.CASCADE)
+    study_id = models.IntegerField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     study_exp = models.FloatField()
     capacity = models.IntegerField()
@@ -23,9 +23,12 @@ class User_Study(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     study_id = models.ForeignKey(Study, on_delete=models.CASCADE)
     date = models.DateTimeField()
+    
+    class Meta:
+        UniqueConstraint(fields=['user_id', 'study_id'], name='user_in_study')
 
 class Item(models.Model):
-    item_id = models.IntegerField(primary_key=models.CASCADE)
+    item_id = models.IntegerField(primary_key=True)
     item_name = models.CharField(max_length=20)
     item_price = models.IntegerField()
     item_type = models.IntegerField()
@@ -34,13 +37,16 @@ class Inventory(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
     buy_time = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        UniqueConstraint(fields=['user_id', 'item_id'], name='item_in_inventory')
 
 class Board(models.Model):
-    board_id = models.IntegerField(primary_key=models.CASCADE)
+    board_id = models.IntegerField(primary_key=True)
     board_name = models.CharField(max_length=20)
 
 class Post(models.Model):
-    post_id = models.IntegerField(primary_key=models.CASCADE)
+    post_id = models.IntegerField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
@@ -52,3 +58,6 @@ class Comment(models.Model):
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     time = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        UniqueConstraint(field=['user_id', 'post_id'], name='user_post')
