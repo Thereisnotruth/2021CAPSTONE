22ebe9e7e190a3ca0256675e76c4d2ab9c49ec7c
Paulo <thereisnotruth12@gmail.com>
2021-04-18 03:35:41 +0900

    modify models.py

--- backend/helpapp/models.py
+++ backend/helpapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class User(models.Model):
-    user_id = models.IntegerField(primary_key=True)
+    user_id = models.CharField(primary_key=True, max_length=20)
     user_pw = models.CharField(max_length=20)
     user_name = models.CharField(max_length=20)
     gender = models.CharField(max_length=5)
@@ -14,14 +14,21 @@ class User(models.Model):
     point = models.IntegerField()
 
 class Study(models.Model):
-    study_id = models.IntegerField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    study_id = models.BigAutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='host_id')
+    name = models.CharField(max_length=20)
     study_exp = models.FloatField()
     capacity = models.IntegerField()
 
+class Invitation(models.Model):
+    invitation_id = models.BigAutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='to')
+    study_id = models.ForeignKey(Study, on_delete=models.CASCADE, db_column='from')
+
+
 class User_Study(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    study_id = models.ForeignKey(Study, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
+    study_id = models.ForeignKey(Study, on_delete=models.CASCADE, db_column='study_id')
     date = models.DateTimeField()
     
     class Meta:
@@ -30,14 +37,14 @@ class User_Study(models.Model):
         ]
 
 class Item(models.Model):
-    item_id = models.IntegerField(primary_key=True)
+    item_id = models.BigAutoField(primary_key=True)
     item_name = models.CharField(max_length=20)
     item_price = models.IntegerField()
     item_type = models.IntegerField()
 
 class Inventory(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE, db_column='item_id')
     buy_time = models.DateTimeField(auto_now_add=True)
     
     class Meta:
@@ -47,24 +54,20 @@ class Inventory(models.Model):
 
 
 class Board(models.Model):
-    board_id = models.IntegerField(primary_key=True)
+    board_id = models.BigAutoField(primary_key=True)
     board_name = models.CharField(max_length=20)
 
 class Post(models.Model):
-    post_id = models.IntegerField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
+    post_id = models.BigAutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
+    board_id = models.ForeignKey(Board, on_delete=models.CASCADE, db_column='board_id')
     title = models.CharField(max_length=20)
     content = models.CharField(max_length=500) # 글자 제한 500
     time = models.DateTimeField(auto_now_add=True) # 해당 레코드 생성 시 생성 시간 자동 저장
 
 class Comment(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_id = models.BigAutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, db_column='post_id')
     content = models.CharField(max_length=200)
     time = models.DateTimeField(auto_now_add=True)
-    
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['user_id', 'post_id'], name='user_post')
-        ]
