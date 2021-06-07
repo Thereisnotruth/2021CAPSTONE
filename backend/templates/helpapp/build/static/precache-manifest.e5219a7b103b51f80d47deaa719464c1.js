fc98385abf4a59324b96b89b84463f6dc8fe667f
Guhyeon <ghgoo1798@gmail.com>
2021-06-08 02:23:49 +0900

    templates 폴더 추가

---
+++ backend/templates/helpapp/build/static/precache-manifest.e5219a7b103b51f80d47deaa719464c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a983954214294a18759567abdbf2d614",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e63cb5bc40857e6d441",
+    "url": "/static/css/main.639a3321.chunk.css"
+  },
+  {
+    "revision": "e52f501dede18c67b6a4",
+    "url": "/static/js/2.096e44e8.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/static/js/2.096e44e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca61e0a0ff2dea935adf",
+    "url": "/static/js/3.ced5e423.chunk.js"
+  },
+  {
+    "revision": "9e63cb5bc40857e6d441",
+    "url": "/static/js/main.b913c5b0.chunk.js"
+  },
+  {
+    "revision": "ae08d6cfc89c5c6ada02",
+    "url": "/static/js/runtime-main.6bd35ba9.js"
+  }
+]);
