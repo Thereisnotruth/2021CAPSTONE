578c97b0b53af123c56d01ea71cb1c8d0e2a4a3c
Guhyeon <ghgoo1798@gmail.com>
2021-06-08 02:18:40 +0900

    수정파일

--- backend/helpapp/views.py
+++ backend/helpapp/views.py
@@ -110,13 +110,18 @@ def create_user(request):
 @permission_classes((permissions.AllowAny,))
 def show_mygroups(request, user_id):
     if request.method == 'POST':
+        print('request',request)
         user_id = user_id
         user = get_object_or_404(User, user_id=user_id)
+        print('User', user)
         study_set = User_Study.objects.filter(user_id=user_id)
+        print('Study', study_set)
         study_list = []
         for study in study_set:
+            print('Study 엘레먼트', study)
             study_list.append(study.study_id)
         serializer = StudySerializer(study_list, many=True)
+        print(serializer.data)
         return JsonResponse(serializer.data, status=200, safe=False)
     return HttpResponse(status=400)
 
@@ -154,6 +159,7 @@ def study_list(request):
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
 def study_detail(request, study_id):
+    study_list = Study.objects.order_by('study_total_time')[:100]
     study = get_object_or_404(Study, study_id=study_id)
     serializer = StudySerializer(study)
     return JsonResponse(serializer.data, status=200)
