eb9a89be5e8b8719552ae1bbcb45694acb7edca1
Guhyeon <ghgoo1798@gmail.com>
2021-06-07 15:16:12 +0900

    delete front-code

--- frontend/src/test.js
+++
@@ -1,7 +0,0 @@
-import { UnityContext } from 'react-unity-webgl';
-export const unityContext = new UnityContext({
-    loaderUrl: 'static/Build/help.loader.js',
-    dataUrl: 'static/Build/help.data',
-    frameworkUrl: 'static/Build/help.framework.js',
-    codeUrl: 'static/Build/help.wasm'
-});
