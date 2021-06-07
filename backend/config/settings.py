0ea022a9cc6262a47897a9f5b6ab5dcb327980a0
Guhyeon <ghgoo1798@gmail.com>
2021-06-08 00:30:12 +0900

    내 스터디그룹 조회 기능 추가

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -14,14 +14,15 @@ import os
 
 from pathlib import Path
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 import json
 from django.core.exceptions import ImproperlyConfigured
 
-secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# secret_file = os.path.join(BASE_DIR, './secrets.json') # secrets.json 파일 위치를 명시
+secret_file = os.path.join('./secrets.json') # secrets.json 파일 위치를 명시
+
 
 with open(secret_file) as f:
     secrets = json.loads(f.read())
